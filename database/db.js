7cacc163ba4b406903b79bf539c6cf5a7527f460
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-09-29 12:06:15 +0500

    changed host name

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql';
 
 export const db = mysql.createConnection({
-    host: 'us-cdbr-east.cleardb.com',
+    host: 'us-cdbr-east-06.cleardb.net',
     user: 'bed2364a646a7b',
     password: '79dc9c30',
     database: 'heroku_1be9fd2aa4b8c4d',
